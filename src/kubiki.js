58ca259a0677d6b46d8842fac27b04bfaa25f638
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-10-24 19:37:54 +0200

    another break in the wall ♬♫

--- src/kubiki.js
+++ src/kubiki.js
@@ -22,27 +22,32 @@ export function createWalls(room) {
     let z = room.height / 2;
     let angle = 0;
 
-    const createWall = (length, turn) => {
+    const createWall = (length, turn, isBreak = false) => {
         angle += turn;
         const nextX = x + length * Math.cos(angle);
         const nextZ = z + length * Math.sin(angle);
+        if (!isBreak) {
+            vertices.push(...[
+                x, floor, z,
+                nextX, floor, nextZ,
+                x, ceiling, z,
 
-        vertices.push(...[
-            x, floor, z,
-            nextX, floor, nextZ,
-            x, ceiling, z,
-
-            x, ceiling, z,
-            nextX, floor, nextZ,
-            nextX, ceiling, nextZ,
-        ]);
+                x, ceiling, z,
+                nextX, floor, nextZ,
+                nextX, ceiling, nextZ,
+            ]);
+        }
         x = nextX;
         z = nextZ;
     }
     const floor = 0;
     const ceiling = 1;
-
-    createWall(room.width, 0);
+    const length = room.width;
+    const doorOffset = 3;
+    const doorWidth = 1;
+    createWall(doorOffset, 0);
+    createWall(doorWidth, 0, true);
+    createWall(length - doorWidth - doorOffset, 0);
     createWall(room.height, - Math.PI * 0.5);
     createWall(room.width, - Math.PI * 0.5);
     createWall(room.height, - Math.PI * 0.5);

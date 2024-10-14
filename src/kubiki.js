a14a4303ac8eb2d8538600d0ca7fbd7e749e7b0d
Łukasz Lityński <lukasz.litynski@gmail.com>
2024-10-15 00:22:40 +0200

    render multiple objects

--- src/kubiki.js
+++ src/kubiki.js
@@ -5,14 +5,16 @@ export function init(params) {
 
 const vertexShaderSource = `
 attribute vec2 aPosition;
+uniform vec2 uPosition;
+#define SCALE 0.5
 void main() {
-	gl_Position = vec4(aPosition, 0.0, 1.0);
+	gl_Position = vec4((aPosition + uPosition) * SCALE, 0.0, 1.0);
 }
 `;
 
 const fragmentShaderSource = `
 void main() {
-	gl_FragColor = vec4(0.1, 0.0, 1.0, 1.0);
+	gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
 }
 `;
 
@@ -39,6 +41,22 @@ function initWebGL(gl) {
 	return { program };
 }
 
+const triangleGeometry = new Float32Array([
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+]);
+
+const boxGeometry = new Float32Array([
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+	0.0, 0.0,
+	1.0, 1.0,
+	0.0, 1.0,
+]);
+
+
 class Kubiki {
 	constructor(params) {
 		params = structuredClone(params);
@@ -53,6 +71,11 @@ class Kubiki {
 		this.program = program;
 		this.gl = gl;
 		this.params = params;
+		this.objects = [
+			{position: [0, 0], geometry: boxGeometry},
+			{position: [0, -1.3], geometry: triangleGeometry},
+			{position: [-1.3, 0], geometry: boxGeometry},
+		];
 	}
 	mount(domEl) {
 		console.log("mounted", domEl, this.canvas)
@@ -64,18 +87,21 @@ class Kubiki {
 		gl.clearColor(...params.background);
 		gl.clear(this.gl.COLOR_BUFFER_BIT);
 
-		const aPosition = gl.getAttribLocation(this.program, 'aPosition');
-		const buffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-			0.0, 0.0,
-			1.0, 0.0,
-			1.0, 1.0,
-		]), gl.STATIC_DRAW);
-		gl.enableVertexAttribArray(aPosition);
-		gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-		gl.useProgram(this.program)
-		gl.drawArrays(gl.TRIANGLES, 0, 3);
+		this.objects.forEach(obj => {
+			const dimensions = 2;
+			const aPosition = gl.getAttribLocation(this.program, 'aPosition');
+			const uPosition = gl.getUniformLocation(this.program, 'uPosition');
+			gl.useProgram(this.program);
+			if (dimensions == 2) {
+				gl.uniform2f(uPosition, ...obj.position);
+			} else throw new Error('not implemented for dimensions != 2');
+			const buffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			gl.bufferData(gl.ARRAY_BUFFER, obj.geometry, gl.STATIC_DRAW);
+			gl.enableVertexAttribArray(aPosition);
+			gl.vertexAttribPointer(aPosition, dimensions, gl.FLOAT, false, 0, 0);
+			gl.drawArrays(gl.TRIANGLES, 0, obj.geometry.length / dimensions);
+		});
 		return this;
 	}
 }

95bd3d28edbd9bdb26ad3ad76027ef4601955983
Łukasz Lityński <lukasz.litynski@gmail.com>
2024-10-15 21:00:26 +0200

    lookAt()

--- src/SceneObject.js
+++ src/SceneObject.js
@@ -5,6 +5,7 @@ export class SceneObject {
 		position: [0, 0, 0, 1],
 		scale: [1, 1, 1],
 		rotation: [0, 0, 0],
+		target: null,
 		matrix: mat4.create(),
 	};
 	constructor(geometry) {
@@ -26,13 +27,22 @@ export class SceneObject {
 	scale(x, y, z) {
 		return this.setTransformItem('scale', x, y, z);
 	}
+	lookAt(x, y, z) {
+		this.transform.target = [x, y, z];
+		return this;
+	}
 	computeMatrix() {
 		const matrix = this.transform.matrix;
 		mat4.identity(matrix);
 		mat4.scale(matrix, matrix, this.transform.scale);
 		mat4.translate(matrix, matrix, this.transform.position);
-		mat4.rotateX(matrix, matrix, this.transform.rotation[0]);
-		mat4.rotateY(matrix, matrix, this.transform.rotation[1]);
-		mat4.rotateZ(matrix, matrix, this.transform.rotation[2]);
+		if (this.transform.target) {
+			const up = [0, 1, 0];
+			mat4.targetTo(matrix, this.transform.position.slice(0, 3), this.transform.target, up);
+		} else {
+			mat4.rotateX(matrix, matrix, this.transform.rotation[0]);
+			mat4.rotateY(matrix, matrix, this.transform.rotation[1]);
+			mat4.rotateZ(matrix, matrix, this.transform.rotation[2]);
+		}
 	}
 }
--- src/example.js
+++ src/example.js
@@ -10,15 +10,16 @@ const kubiki = K.init({width: 1024, height: 768, background: [0, 0, 0.4, 1]})
 	// .add(K.triangle())
 	// .add(K.triangle().position(0, -2.0));
 
+kubiki.camera.lookAt(0, 0, 0);
 function update(t) {
 	rect.rotation(x => x + 0.04, y => y, z => z);
 	// box.position(x => Math.cos(t * 0.002) * 4, y => Math.sin(t * 0.003) * 5, z => z);
 	// rect.scale(x => x + 0.01, y => y + 0.01, 1);
 	kubiki.render(t);
-	box.rotation(x => x, y => y + 0.01, z => z + 0.02)
+	// box.rotation(x => x, y => y + 0.01, z => z + 0.02)
 	const angle = t * 0.001;
 	const radius = 10;
-	// kubiki.camera.position(Math.cos(angle) * radius, 0, Math.sin(angle) * radius)
+	kubiki.camera.position(Math.cos(angle) * radius, 0, Math.sin(angle) * radius);
 	// kubiki.camera.transform.rotation[1] = Math.sin(t * 0.001) * 0.3;
 	// kubiki.camera.rotation(x => x, y => Math.sin(t * 0.001) * 0.3, z => z);
 	requestAnimationFrame(update);

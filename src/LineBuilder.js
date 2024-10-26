7c5692509cf10c0b839b6223dfeea5e8d6d99da1
Łukasz Lityński <lukasz.litynski@gmail.com>
2024-10-26 16:14:20 +0200

    LineBuilder: .lines, .hidden()/visible(), fluent interface

--- src/LineBuilder.js
+++ src/LineBuilder.js
@@ -1,13 +1,33 @@
 export class LineBuilder {
 	points = [[0, 0]];
+	lines = [];
 	rotation = 0;
 	cursor = [0, 0];
+	isHidden = false;
 	forward(amount) {
+		const from = [...this.cursor];
 		this.cursor[0] += Math.cos(this.rotation) * amount;
 		this.cursor[1] += Math.sin(this.rotation) * amount;
-		this.points.push([...this.cursor]);
+		const to = [...this.cursor];
+		if (!this.isHidden) {
+			this.points.push(to);
+			this.lines.push({
+				from,
+				to,
+			});
+		}
+		return this;
 	}
 	turn(angle) {
 		this.rotation += angle;
+		return this;
+	}
+	hidden() {
+		this.isHidden = true;
+		return this;
+	}
+	visible() {
+		this.isHidden = false;
+		return this;
 	}
 }
--- test/LineBuilder.test.js
+++ test/LineBuilder.test.js
@@ -4,10 +4,19 @@ import { LineBuilder } from '../src/LineBuilder.js';
 describe('LineBuilder', () => {
 	it('initial state', () => {
 		const builder = new LineBuilder();
+		assert.deepStrictEqual(builder.lines, []);
 		assert.deepStrictEqual(builder.points, [[0,0]]);
 		assert.deepStrictEqual(builder.rotation, 0);
 		assert.deepStrictEqual(builder.cursor, [0, 0]);
 	});
+	it("methods should return 'this' to enable fluent interface", () => {
+		const builder = new LineBuilder();
+		assert.strictEqual(builder.forward(1), builder);
+		assert.strictEqual(builder.turn(0.123), builder);
+		assert.strictEqual(builder.hidden(), builder);
+		assert.strictEqual(builder.visible(), builder);
+	});
+
 	it('forward', () => {
 		const builder = new LineBuilder();
 
@@ -15,11 +24,13 @@ describe('LineBuilder', () => {
 		assert.deepStrictEqual(builder.rotation, 0);
 		assert.deepStrictEqual(builder.cursor, [1.5, 0]);
 		assert.deepStrictEqual(builder.points, [[0, 0], [1.5, 0]]);
+		assert.deepStrictEqual(builder.lines, [{from: [0, 0], to: [1.5, 0]}]);
 
 		builder.forward(2.5);
 		assert.deepStrictEqual(builder.rotation, 0);
 		assert.deepStrictEqual(builder.cursor, [4, 0]);
 		assert.deepStrictEqual(builder.points, [[0, 0], [1.5, 0], [4, 0]]);
+		assert.deepStrictEqual(builder.lines, [{from: [0, 0], to: [1.5, 0]}, {from: [1.5, 0], to: [4, 0]}]);
 	});
 
 	it('turn', () => {
@@ -49,6 +60,19 @@ describe('LineBuilder', () => {
 		assert.deepStrictEqual(builder.rotation, angle);
 		assert.deepStrictEqual(builder.cursor, [Math.cos(angle) * 1.5, Math.sin(angle) * 1.5]);
 		assert.deepStrictEqual(builder.points, [[0, 0], [Math.cos(angle) * 1.5, Math.sin(angle) * 1.5]]);
+	});
+
+	it('hidden & visible', () => {
+		const builder = new LineBuilder();
+		builder.forward(1.5);
+		builder.hidden();
+		builder.forward(2.5);
+		builder.visible();
+		builder.forward(1);
+		assert.deepStrictEqual(builder.lines, [
+			{from: [0, 0], to: [1.5, 0]},
+			{from: [4, 0], to: [5, 0]},
+		]);
 	});
 });
 
